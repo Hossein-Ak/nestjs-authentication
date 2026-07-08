6d132b0c8a6545514f89aefb1f3174c9184c3c09
Hosseinreza <hrakbari77@gmail.com>
2026-07-08 09:50:18 +0330

    Add user service and module

---
+++ prisma/prisma.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client.js';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const dbUrl = new URL(process.env.DATABASE_URL!);
+
+    const adapter = new PrismaMariaDb({
+      host: dbUrl.hostname,
+      port: Number(dbUrl.port) || 3306,
+      user: dbUrl.username,
+      password: dbUrl.password,
+      database: dbUrl.pathname.slice(1), // remove leading "/"
+    });
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
